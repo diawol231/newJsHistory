6b75e8ad9eab349aadd2b1882bcd385748ba1260
Mark <foratik2001@gmail.com>
2020-06-21 16:58:02 +0700

    Женек, что за ..

---
+++ mark/ka.js
@@ -0,0 +1,38 @@
+'esversion: 6';
+/*
+
+let number = 4;
+const leftBorderWitj = 5;
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+obj.a = 10;
+console.log(obj);
+const lol = {
+    name: "Mark",
+    age: 24,
+    Prise: 2000
+};
+console.log(lol.Prise);
+let arr = ["2", 5, {}, []];
+for(let i = 0; i < 4; i++){
+console.log(arr[i]);
+}
+
+alert("HELLO");
+const result = confirm("Are u 0k");
+const answer = +prompt("Вам есть 18?", "20");
+console.log(answer+5);
+const answer = [];
+answer[0] = prompt("Как ваше имя?", "");
+answer[1] = prompt("Как ваше фамилия?", "");
+answer[2] = prompt("Сколько вам лет?", "");
+console.log(typeof(answer));
+
+const cotigory= 'toys';      //интерполяция
+console.log(`https://someutr.com/${cotigory}/5`);*/
+const name = "Ivan";
+alert(`Hi,${name}!`);
---
+++ scrips.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log(4 + '5');
+console.log(2 * 4 === 2);
+const a = 4,
+    b = 9;
+console.log(a || b);
